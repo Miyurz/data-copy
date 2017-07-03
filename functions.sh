7782f115a3c021eacc48347cc76cc7b73faf3355
MiyurZ <mayur.nagekar@gmail.com>
2017-07-03 14:30:07 +0530

    Fix

--- functions.sh
+++ functions.sh
@@ -3,12 +3,13 @@
 download_location=$(pwd)
 
 mkdir -p "/tmp"/{"node1","node2","node3","node4","node5"}
+fileSizeToDownload=10GB
 
 function downloadFromSlowSource() {
 
-  fileSizeToDownload=10GB
-  figlet Download Status
-  wget  http://speedtest.tele2.net/"${fileSizeToDownload}".zip --directory-prefix=${download_location}
+  echo Download Status
+  rm -rf ${fileSizeToDownload}.*
+  wget --no-verbose http://speedtest.tele2.net/"${fileSizeToDownload}".zip --directory-prefix=${download_location}
 }
 
 
@@ -16,8 +17,8 @@ function copyPeerToPeer() {
 
  copyToNode=$1
  fromNode=$download_location
- figlet Copying Status from $fromNode to $copyToNode
- rsync -azPv ${download_location} ${copyToNode}/
+ echo Copying Status for $(basename $copyToNode)
+ rsync -azPv ${download_location}/${fileSizeToDownload}.zip  ${copyToNode}/
  #rsync -a ~/dir1 username@remote_host:destination_directory
 
 }
